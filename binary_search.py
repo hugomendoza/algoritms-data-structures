da30678472c3eb255d0483fa7ec44f2da72dde1b
Hugo_Mendoza <hugo_mendoza_s@outlook.com>
2023-02-28 10:50:03 -0500

    feat: algoritms data structure

---
+++ binary_search.py
@@ -0,0 +1,30 @@
+def binary_search(list, target):
+  first = 0
+  last = len(list) - 1
+
+  midpoint = (first + last) // 2
+  print(midpoint)
+  while first <= last:
+
+    if list[midpoint] == target:
+      return midpoint
+    elif list[midpoint] < target:
+      first = midpoint + 1
+    else:
+      last = midpoint - 1
+  
+  return None
+
+def verify(index):
+  if index is not None:
+    print("Target found at index: ", index)
+  else:
+    print("Target not found in list")
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+result = binary_search(numbers, 12)
+verify(result)
+
+result = binary_search(numbers, 6)
+verify(result)
---
+++ linked_list.py
@@ -0,0 +1,157 @@
+from platform import node
+
+
+class Node:
+  """
+    An object for storing a single node of a linked list.
+    Models two attributes - data and the link to the next node in the list
+  """
+
+  data = None
+  next_node = None
+
+  def __init__(self, data):
+    self.data = data
+
+  def __repr__(self):
+    return "<Node data: %s>" % self.data
+
+class LinkedList:
+
+  def __init__(self):
+    self.head = None
+
+  def is_empty(self):
+    return self.head == None
+
+  def size(self):
+
+    """
+    Returns the number of nodes in the list
+    Takes 0(n) == Linear search time
+    """
+
+    current = self.head
+    count = 0
+
+    while current:
+      count += 1
+      current = current.next_node
+    
+    return count
+
+  def add(self, data):
+
+    """
+    Adds new Node containing data at head og the lis
+    Takes 0(1) time === Constant time
+    """
+
+    new_node = Node(data)
+    new_node.next_node = self.head  # type: ignore
+    self.head = new_node
+
+  def search(self, key):
+
+    """
+    Search for the first Node containing data that matches the key
+    Return the nide or "None" if not found
+    Takes 0(n) == Linear search time
+    """
+
+    current = self.head
+
+    while current:
+      if current.data == key:
+        return current
+      else:
+        current = current.next_node
+
+    return None
+
+  def insert(self, data, index):
+
+    """
+    Inserts a new Node containing data at index position
+    Insertion takes 0(1) time but finding the node at the
+    insertion point takes 0(n) time
+
+    Takes overall 0(n) time
+    """
+    
+    if index == 0:
+      self.add(data)
+    
+    if index > 0:
+      new = Node(data)
+
+      position = index
+      current = self.head
+      
+      while position > 1:
+        current = node.next_node
+        position -= 1
+      
+      prev_node = current
+      next_node = current.next_node # type: ignore
+
+      prev_node.next_node = new  # type: ignore
+      new.next_node = next_node
+
+  def remove(self, key):
+
+    """
+    Removes Node containing data that matches the key
+    Returns the node or None if ket doesn't exist
+    Takes O(n) time
+    """
+
+    current = self.head
+    previous = None
+    found = False
+
+    while current and not found:
+      if current.data == key and current is self.head:
+        found = True
+        self.head = current.next_node
+      elif current.data == key:
+        found = True
+        previous.next_node = current.next_node # type: ignore
+      else:
+        previous = current
+        current = current.next_node
+
+    return current
+
+  def node_at_index(self, index):
+    if index == 0:
+      return self.head
+    else:
+      current = self.head
+      position = 0
+
+      while position < index:
+        current = current.next_node # type: ignore
+        position += 1
+      
+      return current
+
+  def __repr__(self):
+
+    """
+    Return a string representation of the list
+    Takes O(n) time
+    """
+    nodes = []
+    current = self.head
+
+    while current:
+      if current is self.head:
+        nodes.append("[Head: %s]" % current.data)
+      elif current.next_node is None:
+        nodes.append("[Tail: %s]" % current.data)
+      else:
+        nodes.append("[%s]" % current.data)
+
+      current = current.next_node
+    return '-> '.join(nodes)
